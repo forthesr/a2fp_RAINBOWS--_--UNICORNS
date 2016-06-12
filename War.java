48b55203d34964c48e4629a542441cb775efab79
Jordan Louie <Jordan Louie>
2016-06-12 19:49:42 -0400

    War finish

--- War.java
+++ War.java
@@ -7,15 +7,17 @@ import java.util.*;
 
 public class War {
 
-    private boolean fin = false;
+    private static boolean fin = false;
+    private Deck _winPile = new Deck();
 
     //players' hands
-    private Deck _p1Play;
-    private Deck _p2Play;
+    private Deck _p1Play = new Deck();
+    private Deck _p2Play = new Deck();
 
     //players' win piles
-    private Deck _p1Win;
-    private Deck _p2Win;
+    private Deck _p1Win = new Deck();
+    private Deck _p2Win = new Deck();
+
     
     public War(){
 	
@@ -27,14 +29,16 @@ public class War {
 
 	System.out.println("Distributing cards");
 	//splits the deck
-	while (blah.getSize() > 0){
+	for (int i = 0; i < 52; i+= 2){
 	    _p1Play.drawFrom(blah, 0);
 	    _p2Play.drawFrom(blah, 0);
 	}
+
+	System.out.println("P1: " + _p1Play + "\nP2: " + _p2Play);
     }
 
     public void play(){
-	Deck temp;
+	Deck temp = _p1Play;
 	if (_p1Play.isEmpty()){
 	    if (_p1Win.isEmpty()){
 		loseGame();
@@ -43,7 +47,7 @@ public class War {
 		swapDeck(_p1Win, _p1Play);
 	    }
 	}
-	else if (_p2Play.isEmpty()){
+	if (_p2Play.isEmpty()){
 	    if (_p2Win.isEmpty()){
 		winGame();
 	    }
@@ -53,34 +57,43 @@ public class War {
 	}
 	
 	System.out.println ("Declare War? (y): ");
-	int ans = Keyboard.readString();
-	System.out.println("You: " + _p1Play.peekCard(0) + "/n" +
+	String ans = Keyboard.readString();
+	System.out.println("You: " + _p1Play.peekCard(0) + "\n" +
 			   "CPU: " + _p2Play.peekCard(0));
+	    
 	if(_p1Play.peekCard(0) > _p2Play.peekCard(0)){
+	    System.out.println("You won this round!");
 	    temp = _p1Win;
-	    temp.drawFrom(_p1Play, 0);
-	    temp.drawFrom(_p2Play, 0);
+	    _winPile.drawFrom(_p1Play, 0);
+	    _winPile.drawFrom(_p2Play, 0);
 	}
 	else if (_p1Play.peekCard(0) < _p2Play.peekCard(0)){
+	    System.out.println("You lost this round");
 	    temp = _p2Win;
-	    temp.drawFrom(_p1Play, 0);
-	    temp.drawFrom(_p2Play, 0);
+	    _winPile.drawFrom(_p1Play, 0);
+	    _winPile.drawFrom(_p2Play, 0);
 	}
 	else{
 	    System.out.println("You have both played the same card, " + 
 			       "play another");
+	    _winPile.drawFrom(_p1Play, 0);
+	    _winPile.drawFrom(_p2Play, 0);
 	    this.play();
-	    temp.drawFrom(_p1Play, 0);
-	    temp.drawFrom(_p2Play, 0);
 	}
-	    
+
+	swapDeck(_winPile, temp);
+
+	System.out.println("You: " + _p1Win + "\n" +
+			   "CPU: " + _p2Win);
 	
     }
 
     public void swapDeck(Deck win, Deck play){
-	Deck temp = play;
+	while (win.getSize() > 0)
+	    play.drawFrom(win, 0);
+	/*Deck temp = play;
 	play  = win;
-	win = temp;
+	win = temp;*/
 	play.shuffle();
     }
 
@@ -94,7 +107,7 @@ public class War {
 	fin = true;
     }
 
-    public void Main(String args){
+    public static void main(String[] args){
 	//War p1 = new War();
 	War game = new War();
 	while (fin != true)
