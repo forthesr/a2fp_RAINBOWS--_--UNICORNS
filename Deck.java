ce504d300327e5b2cb216289ba32c328ea7a8a47
Ling <lcheng3@stuy.edu>
2016-06-12 20:12:42 -0400

    Fixed Deck

--- Deck.java
+++ Deck.java
@@ -21,11 +21,7 @@ public class Deck{
 		    _deck.add(c);
 		}
 	    }
-	}else{
-	    System.out.println("Wrong number");	
-	}
-	
-	if (num== 10){
+	}else if (num== 10){
 	    for (String s: str){
 		for(int i= 1; i<=10; i++){
 		    Card c= new Card(i, s);
